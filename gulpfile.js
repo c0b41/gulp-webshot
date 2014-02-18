7ffd47d35f5488b36243523710019c96863eba52
cobaimelan <ayhankuru@yandex.com.tr>
2014-02-18 14:01:53 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var
   gulp = require('gulp'),
   connect = require('gulp-connect');
-  gulpshot=require('./index.js');
+  gulpshot=require('./index.js'),
+  mocha = require('gulp-mocha');
 
 gulp.task('connect', connect.server({
   root: ['Theme'],
@@ -12,7 +13,12 @@ gulp.task('connect', connect.server({
 gulp.task('webshot', function() {
   return gulp.src('./Theme/*.html')
         .pipe(gulpshot({ dest:'build/',p:1337}));
-})
+});
+
+gulp.task('mocha', function () {
+    gulp.src('test.js')
+        .pipe(mocha({reporter: 'spec'}));
+});
 
 gulp.task('shot', ['connect', 'webshot']);
 
---
+++ test.js
@@ -0,0 +1,12 @@
+var expect = require('expect.js');
+var fs  = require('fs');
+
+	describe('Webshot Test!!!...', function(){
+		
+		it('Test :1 ', function(done){
+
+			// fs.existsSync('./build/about.png');
+			
+		});
+
+	});
